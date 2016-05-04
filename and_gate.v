3fbc830a188a05f55462db2bdadaea7aae28fdb9
DingjZhang <739400988@qq.com>
2016-05-04 13:24:28 +0800

    Added files via upload

---
+++ and_gate.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:52:00 05/03/2016 
+// Design Name: 
+// Module Name:    and_gate 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module and_gate(
+	input and_a,
+	input and_b,
+	output and_out
+);
+
+assign and_out = and_a & and_b;
+
+
+endmodule
